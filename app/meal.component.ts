6bb46f798e3c1e11ecdfc1d07961fb199fc05072
10igeorge <10izzygeorge@gmail.com>
2016-03-25 12:07:31 -0700

    add note if calorie count is higher than 300

--- app/meal.component.ts
+++ app/meal.component.ts
@@ -6,12 +6,13 @@ import { Meal } from './meal.model';
     inputs: ['meal'],
   template: `
   <div>
-    <ul class="meals">
-      <li>{{meal.food}}</li>
+    <ul class="list-group">
+      <a class="list-group-item">{{meal.food}}<p class="unhealthy" *ngIf="meal.calories > 300">This food is high in calories</p></a>
     </ul>
   </div>
   `
 })
 export class MealComponent {
   public meal: Meal;
+
 }
