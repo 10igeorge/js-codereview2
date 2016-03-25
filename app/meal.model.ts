d9ee3b3138a6332ed90652034fc5a20900784dad
10igeorge <10izzygeorge@gmail.com>
2016-03-25 13:11:59 -0700

    add interface for class

--- app/meal.model.ts
+++ app/meal.model.ts
@@ -1,4 +1,10 @@
-export class Meal {
+interface IMeal {
+  food: string;
+  calories: number;
+  details: string;
+}
+
+export class Meal implements IMeal {
   constructor(public food: string, public calories: number, public details: string){
   }
 }
